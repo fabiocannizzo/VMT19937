ed9c049b16f4f8788bf1861219c4ee51aace4817
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2023-02-07 19:00:19 +0800

    simplifications: do not specify alignment

--- demo.cpp
+++ demo.cpp
@@ -104,21 +104,21 @@ void originalPerformance()
 
 int main()
 {
-    //generateBenchmark();
+    generateBenchmark();
 
-    //testEquivalence<1>();
-    //testEquivalence<4>();
+    testEquivalence<1>();
+    testEquivalence<4>();
 #if MT19937_SIMD_VEC_LEN > 4
-    //testEquivalence<8>();
+    testEquivalence<8>();
 #endif
 
     std::cout << "\n";
 
-    //originalPerformance();
-    //testPerformance<1>();
+    originalPerformance();
+    testPerformance<1>();
     testPerformance<4>();
 #if MT19937_SIMD_VEC_LEN > 4
-    //testPerformance<8>();
+    testPerformance<8>();
 #endif
 
     return 0;
