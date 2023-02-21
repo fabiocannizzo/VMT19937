f4e86f4d4e3c2264f1b6bd761cc305034f4c6dd2
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2023-02-22 00:14:25 +0800

    add method to save a matrix as a C array of char

--- bit_matrix.h
+++ bit_matrix.h
@@ -253,6 +253,19 @@ public:
         txtRowDecoder(os, &Encoder::hexToText);
     }
 
+    template <typename OS>
+    void toArrayChar(OS& os) const
+    {
+        size_t fst = true;
+        os << "{";
+        for (size_t r = 0; r < s_nBitRows; ++r) {
+            const char* p = (const char*)rowBegin(r);
+            for (size_t c = 0; c < s_nBytesPerRow; ++c)
+                os << p[c] << ',';
+        }
+        os << "};";
+    }
+
     size_t nnz() const
     {
         size_t n = 0;
