6a3803313280207ebbc3e67289ceb8cb677111c2
Fabio Cannizzo <fabio.cannizzo@gmail.com>
2023-08-04 00:10:29 +0800

    add MKL VSL to test suite

--- src/perf.cpp
+++ src/perf.cpp
@@ -5,6 +5,10 @@
 #define HAVE_SSE2
 #include "../SFMT-src-1.5.1/SFMT.h"
 
+#ifdef TEST_MKL
+#include "mkl_vsl.h"
+#endif
+
 #include <iostream>
 #include <iomanip>
 #include <chrono>
@@ -22,9 +26,9 @@ const uint64_t nRandomPerf = uint64_t(624) * 16 * 500000;
 extern "C" unsigned long genrand_int32();
 extern "C" void init_by_array(unsigned long init_key[], int key_length);
 
-enum Mode {orig, sfmt, simd};
+enum Mode {orig, sfmt, mkl_mt, mkl_sfmt, simd};
 
-const char* modename[] = {"orig", "sfmt", "simd"};
+const char* modename[] = {"MT19937", "SFMT19937", "VSLMT19937", "VSLSFMT19937", "VMT19937"};
 
 struct Result
 {
@@ -47,13 +51,10 @@ Result testPerformance(size_t runId)
 
     static const size_t BlkSize = gen_t::s_qryBlkSize;
 
-    std::cout << "Generate " << nRandomPerf << " random numbers with SIMD length " << VecLen;
-    if (BlkSize > 1)
-        std::cout << " in blocks of " << BlkSize;
-    std::cout << " ... ";
+    std::cout << "Generate " << nRandomPerf << " random numbers with VMT19937 length " << VecLen
+              << " in blocks of " << BlkSize << " ... ";
 
-    std::vector<uint32_t> dst(BlkSize + 64 / sizeof(uint32_t));
-    uint32_t* aligneddst = (uint32_t*)((intptr_t)dst.data() + (64 - ((intptr_t)dst.data() % 64)));
+    uint32_t* aligneddst = myAlignedNew<uint32_t, 64>(BlkSize);
 
     gen_t mt(seedinit, seedlength, 0, nullptr, nullptr);
 
@@ -72,6 +73,8 @@ Result testPerformance(size_t runId)
     std::chrono::duration<double> elapsed_seconds = end - start;
     double nSeconds = elapsed_seconds.count();
 
+    myAlignedDelete(aligneddst);
+
     std::cout << "done in: " << std::fixed << std::setprecision(2) << nSeconds << "s" << std::endl;
 
     return Result(simd, VecLen, BlkSize, runId, nSeconds);
@@ -80,6 +83,8 @@ Result testPerformance(size_t runId)
 
 Result originalPerformance(size_t runId)
 {
+    std::vector<uint32_t> dst(1);
+
     unsigned long init[seedlength];
     for (size_t i = 0; i < seedlength; ++i)
         init[i] = seedinit[i];
@@ -88,7 +93,7 @@ Result originalPerformance(size_t runId)
     std::cout << "Generate " << nRandomPerf << " random numbers with original code ... ";
     auto start = std::chrono::system_clock::now();
     for (size_t i = 0; i < nRandomPerf; ++i)
-        genrand_int32();
+        dst[0] = genrand_int32();
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     double nSeconds = elapsed_seconds.count();
@@ -99,13 +104,15 @@ Result originalPerformance(size_t runId)
 
 Result sfmtPerformance(size_t runId)
 {
+    std::vector<uint32_t> dst(1);
+
     sfmt_t sfmtgen;
     sfmt_init_gen_rand(&sfmtgen, 12345);
 
     std::cout << "Generate " << nRandomPerf << " random numbers with SFMT code ... ";
     auto start = std::chrono::system_clock::now();
     for (size_t i = 0; i < nRandomPerf; ++i)
-        sfmt_genrand_uint32(&sfmtgen);
+        dst[0] = sfmt_genrand_uint32(&sfmtgen);
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     double nSeconds = elapsed_seconds.count();
@@ -113,6 +120,36 @@ Result sfmtPerformance(size_t runId)
 
     return Result(sfmt, 128, 1, runId, nSeconds);
 }
+
+#ifdef TEST_MKL
+
+template <MKL_INT GenCode, MKL_INT BlkSize>
+Result mklPerformance(size_t runId)
+{
+    uint32_t* aligneddst = myAlignedNew<uint32_t, 64>(BlkSize);
+
+    VSLStreamStatePtr stream;
+    vslNewStream(&stream, GenCode, 5489);
+
+    std::cout << "Generate " << nRandomPerf << " random numbers with MKL "
+              << ((GenCode == VSL_BRNG_MT19937) ? "MT19937" : "SFMT19937")
+              << " in blocks of " << BlkSize << " ... ";
+
+    auto start = std::chrono::system_clock::now();
+    for (size_t i = 0; i < nRandomPerf / BlkSize; ++i)
+        viRngUniformBits32(VSL_RNG_METHOD_UNIFORMBITS32_STD, stream, BlkSize, aligneddst);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    double nSeconds = elapsed_seconds.count();
+    std::cout << "done in: " << std::fixed << std::setprecision(2) << nSeconds << "s\n";
+
+    // Deleting the stream
+    vslDeleteStream(&stream);
+    myAlignedDelete(aligneddst);
+
+    return Result((GenCode == VSL_BRNG_MT19937 ? mkl_mt : mkl_sfmt), 32, BlkSize, runId, nSeconds);
+}
+#endif
 
 void usage()
 {
@@ -145,7 +182,14 @@ int main(int argc, const char** argv)
 
         res.push_back(originalPerformance(i));
         res.push_back(sfmtPerformance(i));
+#ifdef TEST_MKL
+        res.push_back(mklPerformance<VSL_BRNG_MT19937, 1>(i));
+        res.push_back(mklPerformance<VSL_BRNG_MT19937, 16>(i));
+        res.push_back(mklPerformance<VSL_BRNG_SFMT19937, 1>(i));
+        res.push_back(mklPerformance<VSL_BRNG_SFMT19937, 16>(i));
+#endif
         res.push_back(testPerformance<32, QM_Scalar>(i));
+        res.push_back(testPerformance<32, QM_Block16>(i));
         //testPerformance<64>();
         res.push_back(testPerformance<128, QM_Scalar>(i));
         res.push_back(testPerformance<128, QM_Block16>(i));
@@ -172,13 +216,13 @@ int main(int argc, const char** argv)
             iter->time += r.time;
         }
     }
-    std::cout << std::setw(8) << std::right << "method"
+    std::cout << std::setw(8) << std::right << "prng"
         << std::setw(8) << std::right << "n-bits"
         << std::setw(8) << std::right << "blksize"
         << std::setw(8) << std::right << "time"
         << "\n";
     for (auto& r : avgresult) {
-        std::cout << std::setw(8) << std::right << modename[r.mode]
+        std::cout << std::setw(12) << std::right << modename[r.mode]
             << std::setw(8) << std::right << r.nBits
             << std::setw(8) << std::right << r.blkSize
             << std::setw(8) << std::right << std::fixed << std::setprecision(2) << r.time / nRepeat
